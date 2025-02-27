00a7fca400ad880fd386a62ed7751fe3b4e5517b
Vayoni Gamage <141536824+VayoniGamage@users.noreply.github.com>
2025-02-27 17:59:35 +0530

    Update main.dart

--- nic_decoder/lib/main.dart
+++ nic_decoder/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'views/nic_input_screen.dart';
+import 'package:nic_decoder/views/nic_input_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'NIC Decoder',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: NICInputScreen(),
+      debugShowCheckedModeBanner: false,
+      title: "NIC Decoder",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NicInputScreen(),
     );
   }
 }
