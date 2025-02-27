251d90f54ea3bbdbbaadb7909fb03a0d59479119
Vayoni Gamage <141536824+VayoniGamage@users.noreply.github.com>
2025-02-27 17:59:38 +0530

    Create theme.dart

---
+++ nic_decoder/lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFCCDBDC);
+  static const Color button = Color(0xFF567485);
+  static const Color accent = Color(0xFFE7C582);
+  static const Color text = Colors.black;
+  static const Color header = Color(0xFF007EA7);
+  static const Color navBar = Color(0xFF003249);
+}
